aa03768cd3fa19fefd6707bbb98a33805d6de70a
WaterdPlantNua <phenomx497@gmail.com>
2019-08-06 10:07:16 +0100

    Add some code

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,12 +1,32 @@
+let canvas;
+let value = 0;
+let img;
+function preload() {
+  img = loadImage('../src/branding/logo.png');
+}
+
 function setup() {
   // create a canvas
-  createCanvas(600, 800);
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas.mousePressed(clearCanvas)
+  img.loadPixels();
+  
+
 }
 
 function draw() {
-  // colour the background black
-background(0, 160, 250);
+  background(0, 160, 250);
+if (value ===0) {
+  image(img,window.innerWidth/ 2 - 320 , window.innerHeight / 2 - 320, 640, 640);
+  }
   
-  
-  
+
 }
+function clearCanvas() {
+  if (value === 0) {
+  
+  value = 1
+  } 
+}
+
+
