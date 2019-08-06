821d744b6123b9a66fb2f8ef688ac933425518d5
sebastiandoe5 <sebastiandoe5@gmail.com>
2019-08-06 11:20:21 +0100

    Add sliders

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,6 +1,7 @@
 let canvas;
 let value = 0;
 let img;
+
 function preload() {
   img = loadImage('../src/branding/logo.png');
 }
@@ -8,25 +9,46 @@ function preload() {
 function setup() {
   // create a canvas
   canvas = createCanvas(window.innerWidth, window.innerHeight);
-  canvas.mousePressed(clearCanvas)
+  canvas.parent('sketchHolder')
   img.loadPixels();
   
-
+  canvas.mousePressed(() => {
+    if(value === 0) {
+      value = 1;
+      newSlider(10, 50);
+      newProgress(200, 50, '100');
+    }
+  })
 }
 
 function draw() {
   background(0, 160, 250);
-if (value ===0) {
-  image(img,window.innerWidth/ 2 - 320 , window.innerHeight / 2 - 320, 640, 640);
-  }
-  
-
-}
-function clearCanvas() {
   if (value === 0) {
+    image(img, window.innerWidth / 2 - 320, window.innerHeight / 2 - 320, 640, 640);
+    return;
+  }
+  // Full game drawing/logic
   
-  value = 1
-  } 
+
+
 }
 
+function newSlider(x, y) {
+  var slider;
+  slider = createSlider(0, 2, 1);
+  slider.position(x, y);
+  slider.style('width', '80px');
+  slider.parent('sketchHolder');
+  slider.class('speedSlider');
+  return slider;
+}
 
+function newProgress(x, y, max) {
+  var progress = createElement('progress', '100');
+  progress.parent('sketchHolder');
+  progress.position(x, y)
+  progress.class('progress');
+  progress.value('50');
+  progress.attribute('max', max)
+  return progress;
+}
