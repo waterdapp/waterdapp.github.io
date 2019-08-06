84b23aba1e985f4384a0cf5b3a4d656a1529b13b
WaterdPlantNua <phenomx497@gmail.com>
2019-08-06 11:21:50 +0100

    added moving sun prototype

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,3 +1,8 @@
+let gradient = 0
+
+let x = window.innerWidth - window.innerWidth 
+let y = window.innerHeight / 2
+
 let canvas;
 let value = 0;
 let img;
@@ -15,13 +20,47 @@ function setup() {
 }
 
 function draw() {
-  background(0, 160, 250);
-if (value ===0) {
+  var colour = [0, gradient, gradient]
+  background(colour[0], colour[1], colour[2]);
+if (value === 0) {
   image(img,window.innerWidth/ 2 - 320 , window.innerHeight / 2 - 320, 640, 640);
   }
+if(value === 1){
+
+rectMode(CENTER)
+noStroke()
+fill(232, 215, 28)
+ellipse(x, y, 200, 200)
+
+if (x >=  window.innerWidth / 2) {
+  x = x + 02
+  y = y + 0.5
+} else{
+x = x + 0.2
+y = y + -0.05}
+
+
+//x = x + -0.5
+//y = y + 02
+
+
+
+
+
+}
   
 
 }
+
+
+
+
+
+
+
+
+
+
 function clearCanvas() {
   if (value === 0) {
   
